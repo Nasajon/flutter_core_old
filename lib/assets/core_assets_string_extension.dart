e05a59258f9cf3fb308f13328b4f5b04a0c6ecaf
Mauri Couto <mauri.sciensa@genial.com.vc>
2022-08-18 14:00:14 -0300

    fix: change package name from asset

--- lib/assets/core_assets_string_extension.dart
+++ lib/assets/core_assets_string_extension.dart
@@ -4,7 +4,7 @@ extension CoreAssetsStringExtension on String {
   AssetImage get coreImage {
     return AssetImage(
       "assets/images/$this",
-      package: 'core',
+      package: 'flutter_core',
     );
   }
 }
