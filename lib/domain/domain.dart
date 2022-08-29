55b405cc03df7c59ea63a35cd3f40b5ac1d6d9aa
Mauri Couto <mauri.sciensa@genial.com.vc>
2022-08-29 12:15:48 -0300

    flavors

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -1,1 +1,2 @@
 export 'interfaces/interfaces.dart';
+export 'entities/entities.dart';
---
+++ lib/domain/entities/flavor.dart
@@ -0,0 +1,32 @@
+/// Flavors
+// ignore_for_file: constant_identifier_names
+
+enum Flavor {
+  /// dev flavor
+  dev,
+
+  /// hml flavor
+  hml,
+
+  /// preprod flavor
+  preprod,
+
+  /// prod flavor
+  prod,
+}
+
+/// Extension to get name
+extension FlavorDescription on Flavor {
+  /// get flavor name to match with remote config
+  String get remoteConfigName {
+    switch (this) {
+      case Flavor.dev:
+      case Flavor.hml:
+        return name;
+      case Flavor.prod:
+        return 'prd';
+      default:
+        return '';
+    }
+  }
+}
