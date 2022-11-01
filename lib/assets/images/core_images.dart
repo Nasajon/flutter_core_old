98b910fa154cded58d066f4b3e1e7c804c0ef440
mauricouto <maurisilvestre@gmail.com>
2022-11-01 09:43:05 -0300

    Ana Logo

--- lib/assets/images/core_images.dart
+++ lib/assets/images/core_images.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../flutter_core.dart';
 
 abstract class CoreAssets {
+  static AssetImage get anaLogo => "ana_logo.png".coreImage;
   static AssetImage get companyImage => "company.png".coreImage;
   static AssetImage get initialImage => "initial_image.png".coreImage;
   static AssetImage get profilePic => "profile_pic.png".coreImage;
